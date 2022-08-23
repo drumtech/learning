03ddbc01103369474be45c21ef5d226259ef0310
drumtech <41778235+drumtech@users.noreply.github.com>
2022-08-23 12:35:03 +0300

    Update pisos_666.sh

--- pisos_666.sh
+++ pisos_666.sh
@@ -1,1 +1,10 @@
-Pisos
+#!/bin/bash
+### Before you begin make archive from etc for example 'tar cvf etc.tar.gz /etc/'
+### Function will extract archive passed as an argument 1, then will find recursively argument 2 inside unziped directory
+### Construction ${1%.*.*} will cut tar.gz from argument 1
+zzgrep () {
+    tar -xvf $1 > /dev/null
+    grep -r $2 ${1%.*.*}
+}
+
+zzgrep etc.tar.gz passwd
